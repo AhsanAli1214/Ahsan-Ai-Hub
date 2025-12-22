cd4f4c3437fe04db9bcae59015fa51dc69219f02
AhsanAli1214 <192888026+AhsanAli1214@users.noreply.github.com>
2025-12-22 08:21:50 +0000

    https://play-lh.googleusercontent.com/YPKHdRXNI7sIoImuW0YWRmsK9nqSeVv4nE

--- next.config.ts
+++ next.config.ts
@@ -32,6 +32,12 @@ const nextConfig: NextConfig = {
         hostname: 'i.postimg.cc',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'play-lh.googleusercontent.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
