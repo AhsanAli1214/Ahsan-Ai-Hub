2d5749bd8a58fc1f13bc56221804896d85d0a7f8
ahsanaliwadani4 <51817244-ahsanaliwadani4@users.noreply.replit.com>
2025-12-24 11:35:58 +0000

    Restored to '68390963137eafe9775b3732574408b1120fefea'
    Replit-Restored-To: 68390963137eafe9775b3732574408b1120fefea

--- sentry.server.config.ts
+++
@@ -1,9 +0,0 @@
-import * as Sentry from "@sentry/nextjs";
-
-Sentry.init({
-  dsn: process.env.SENTRY_DSN || "https://examplePublicKey@o0.ingest.sentry.io/0",
-  tracesSampleRate: 1.0,
-  sampleRate: 1.0,
-  environment: process.env.NODE_ENV,
-  telemetry: false,
-});
