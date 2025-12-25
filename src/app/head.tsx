c032be205b029521e191ba942f5e851206b409ca
Ahsan Ali <192888026+AhsanAli1214@users.noreply.github.com>
2025-12-25 19:06:09 +0500

    Add files via upload

---
+++ src/app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="google-site-verification" content="iWa7XKOKgZItPEm8pI9yBA1jZ6ys-IbVdyIuebevcmY" />
+      {/* Other global head tags (charset, viewport, favicon, etc.) can go here too */}
+    </>
+  );
+}
